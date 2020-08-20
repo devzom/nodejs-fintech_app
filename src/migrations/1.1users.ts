4faa2edc0a80f94851a97c43303efefac2667cbe
Jacob <dev.zomerfeld@gmail.com>
2020-08-20 22:24:34 +0200

    update DB migrations

--- src/migrations/1.1users.ts
+++ src/migrations/1.1users.ts
@@ -1,33 +1,42 @@
-import * as Sequelize from 'Sequelize';
+import * as Sequelize from 'sequelize';
 
 const tableName = 'Users';
 
-export async function up(params:any) {
-    const queryInterface  = params.getQueryInterface() as Sequelize.QueryInterface;
-    queryInterface.createTable(tableName,{
-        id:{
-            type: Sequelize.INTEGER,
-            allowNull:false,
-            autoIncrement: true,
-            unique: true,
-            primaryKey: true,
-        },
-        Username:{
-            type: Sequelize.CHAR(200),
-            allowNull: false,
-        },
-        Email:{
-            type: Sequelize.CHAR(50),
-            allowNull: false,
-        },
-        Password:{
-            type: Sequelize.CHAR(200),
-            allowNull: false,
-        }
-    })
-}
-
-export async function down(params:any) {
-    const queryInterface  = params.getQueryInterface() as Sequelize.QueryInterface;
-    queryInterface.dropTable(tableName);
+export async function up(param: any) {
+  const queryInterface = param.getQueryInterface() as Sequelize.QueryInterface;
+  queryInterface.createTable(tableName, {
+    id: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      unique: true,
+      primaryKey: true,
+    },
+    Username: {
+      type: Sequelize.CHAR(200),
+      allowNull: false,
+    },
+    Email: {
+      type: Sequelize.CHAR(50),
+      allowNull: false,
+    },
+    Password: {
+      type: Sequelize.CHAR(250),
+      allowNull: false,
+    },
+    Salt: {
+      type: Sequelize.CHAR(250),
+      allowNull: true,
+    },
+    createdAt: {
+      type: Sequelize.DATE,
+    },
+    updatedAt: {
+      type: Sequelize.DATE,
+    }
+  });
+};
+export async function down(param: any) {
+  const queryInterface = param.getQueryInterface() as Sequelize.QueryInterface;
+  queryInterface.dropTable(tableName);
 }
--- src/migrations/1.2accounts.ts
+++ src/migrations/1.2accounts.ts
@@ -1,40 +1,45 @@
-import * as Sequelize from 'Sequelize';
+import * as Sequelize from 'sequelize';
 
 const tableName = 'Accounts';
 
-export async function up(params:any) {
-    const queryInterface  = params.getQueryInterface() as Sequelize.QueryInterface;
-    queryInterface.createTable(tableName,{
-        id:{
-            type: Sequelize.INTEGER,
-            allowNull:false,
-            autoIncrement: true,
-            unique: true,
-            primaryKey: true,
-        },
-        Type:{
-            type: Sequelize.CHAR(200),
-            allowNull: false,
-        },
-        Name:{
-            type: Sequelize.CHAR(200),
-            allowNull: false,
-        },
-        Balance:{
-            type: Sequelize.INTEGER,
-            allowNull: true,
-        },
-        UserId:{
-            type: Sequelize.INTEGER,
-            references:{
-                model: 'Users',
-                key: 'id',
-            }
-        }
-    })
-}
-
-export async function down(params:any) {
-    const queryInterface  = params.getQueryInterface() as Sequelize.QueryInterface;
-    queryInterface.dropTable(tableName);
+export async function up(param: any) {
+  const queryInterface = param.getQueryInterface() as Sequelize.QueryInterface;
+  queryInterface.createTable(tableName, {
+    id: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      unique: true,
+      primaryKey: true,
+    },
+    Type: {
+      type: Sequelize.CHAR(200),
+      allowNull: false,
+    },
+    Name: {
+      type: Sequelize.CHAR(200),
+      allowNull: false,
+    },
+    Balance: {
+      type: Sequelize.INTEGER,
+      allowNull: true,
+    },
+    UserId: {
+      type: Sequelize.INTEGER,
+      references: {
+        model: 'Users',
+        key: 'id',
+      },
+    },
+    createdAt: {
+      type: Sequelize.DATE,
+    },
+    updatedAt: {
+      type: Sequelize.DATE,
+    }
+  });
+};
+export async function down(param: any) {
+  const queryInterface = param.getQueryInterface() as Sequelize.QueryInterface;
+  queryInterface.dropTable(tableName);
 }
