f7f389ebbd2987f50e11000c92ec11bc52b36754
Jacob <dev.zomerfeld@gmail.com>
2020-08-29 11:40:57 +0200

    Fix views paths

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join, resolve } from 'path';
+import * as hbs from 'hbs';
 
 //in purpose to read root .env file
-require('dotenv').config()
-
+require('dotenv').config();
 //declaration for adding any module by const
 declare const module: any;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    logger: ['error', 'debug']
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    // logger: ['error', 'debug'],
   });
+
+  //view
+  app.useStaticAssets(resolve('./src/public'));
+  app.setBaseViewsDir(resolve('./src/views'));
+  hbs.registerPartials(join('./src/views/partials'));
+
+  app.setViewEngine('hbs');
+
+  app.set('view options', {
+    layout: 'layouts/layout',
+  });
+  //view
+
   await app.listen(3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
+
   //for adding Hot-Reload feature
   if (module.hot) {
     module.hot.accept();
